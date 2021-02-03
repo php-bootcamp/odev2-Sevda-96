2bfe070b8206a6a1dcd7255ecffcd77ae9e387ee
Sevda Engin <53494300+Sevda-96@users.noreply.github.com>
2021-02-03 14:55:04 +0300

    Update app.php

--- app.php
+++ app.php
@@ -1,57 +1,70 @@
-<form action="odev1.php" method="post">
+<form action="odev2.php" method="post">
+    <label for="sex">Cinsiyet seçiniz:</label>
+    <select id="sex" name="sex">
+        <option value="seçiniz">seçiniz</option>
+        <option value="Kadın">Kadın</option>
+        <option value="Erkek">Erkek</option>
+    </select><br /><br />
+    <label>Kilo:</label>
+    <input type="text" name="weight" /><br /><br />
+    <label>Boy:</label>
+    <input type="text" name="height" /><br /><br />
     <label>Yas:</label>
-    <input type="text" name="yas" />
+    <input type="text" name="age" /><br /><br />
+
     <input type="submit" name="submit" />
 </form>
 
 <?php
-//$age = 25;
-
 /**
- * Bu ödevde age değişkeni içerisinde yer alan yaş değerinin bulunduğu
- * yaş grubu tespit edilip ekrana bu yaş grubunun ismi yazılmalıdır. 
+ * Bu ödevde günlük kalori hesaplaması yapacağız.
+ * Yukarıdaki değişkenleri ve aşağıdaki formülü kullanarak
+ * kişinin günlük kaç kalori alması gerektiğini hesaplayıp
+ * bunu ekrana yazan PHP kodunu oluşturmamız gerekiyor.
  * 
- * 0-14 = Çocuk
- * 15-24 = Genç
- * 25-64 = Yetişkin
- * 65+ = Yaşlı
+ * Erkekler için; 10 x kilo + 6.25 x boy - 5 x yaş + 5
+ * Kadınlar için; 10 x kilo + 6.25 x boy - 5 x yaş - 161
  * 
- * Ekran çıktısı için `echo` yada `print` kullanabilirsiniz.
+ * Örneğin;
+ * $sex = "female";
+ * $weight = 60;
+ * $height = 170;
+ * $age = 30;
  * 
- * > **Not**: Çıktıda sadece `Çocuk`, `Genç`, `Yetişkin` veya `Yaşlı`
- * yer almalıdır.
- * 
- * İyi çalışmalar.
+ * Ekrana 1326.5 yazması gerekiyor.
  */
 if(isset($_POST['submit'])){
-if(!empty($_POST['yas'])){
-    $yas=$_POST['yas'];
-    //var_dump($yas); 
-    $age=(int)$yas;
-    
-    switch($age){
-        case($age>=0 && $age<=14):
-          echo"<h1>Çocuk</h1>"."\n";
-          break;
-          case($age<=24 && $age>=15):
-              echo"<h1>Genç</h1>"."\n";
-              break;
-              case($age<=64 && $age>=25):
-                  echo"<h1>Yetişkin</h1>"."\n";
-                  break;
-                  case($age>=65):
-                      echo"<h1>Yaşlı</h1>"."\n";
-                      break;
-                      default:
-                      break;
-  
+    if(!empty($_POST['sex']) && !empty($_POST['weight']) && !empty($_POST['height']) && !empty($_POST['age'])){
+        
+        $sex = $_POST['sex']; // cinsiyet (male/female)
+        $weight = $_POST['weight']; // kilo (kg)
+        $height =  $_POST['height']; // boy (cm)
+        $age =  $_POST['age']; // yaş (sene)
+
+        $weight=$weight;
+        $height=$height;
+        $age=(int)$age;
+        
+        switch($sex){
+            case($sex=="Kadın"):
+                $sonuc=(10*$weight) + (6.25*$height)- (5*$age) +5;
+                echo"<h1>Kadın için hesaplanan sonuç:".$sonuc."</h2>";
+                break;
+                case($sex=="Erkek"):
+                   $sonuc2=(10 * $weight) + (6.25 * $height)- (5*$age) +161;
+                   echo"<h1>Erkek için hesaplanan sonuç:".$sonuc2."</h2>";
+                    break;
+                    default:
+                    echo"";
+                    break; 
+                
+        }
+
+    }
+    else{
+        echo"tüm boşlukları doldurunuz!!";
     }
 }
-else{
-    echo"hata boş bırakılamaz!";
-}
-}
 
- 
 
-?>
+ ?>
